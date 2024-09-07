4fe52f12cd9fd404de7591335d3cac4f9a142e99
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-09-07 19:26:26 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.decomposition import PCA
+import gensim.downloader as api
+from util import *
+
+# Initialize streamlit app
+page_title = "Visualizing Word Embeddings"
+page_icon = "✨️"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout="centered")
+
+# Application title
+st.title(page_title)
+st.write(':blue[***Visualize the Semantic Universe of Words 🧠🌍***]')
+st.write("Explore the fascinating world of word embeddings! Input words and see how they align in vector space. The "
+         "closer they are, the more similar they are in meaning. Unlock insights into language relationships with this "
+         "interactive tool! 🔍📊")
+
+st.subheader('Enter Words:')
+words = st.text_input("Enter at least two words to visualize them. Words should be comma separated.",
+                      value='king, queen')
+# Split the string by commas and remove any extra spaces
+word_list = [word.strip().lower() for word in words.split(',') if word]
+if len(word_list) < 2:
+    st.error('Enter at least two words')
+else:
+    st.subheader('Visualize:')
+    visualize = st.button('Visualize')
+    if visualize:
+        with st.spinner('Processing ...'):
+            # Load the GloVe word Embeddings
+            word_vectors = api.load('glove-wiki-gigaword-100')
+
+            # Get word vectors
+            vectors = np.array([word_vectors[word] for word in word_list])
+
+            # Reduce dimensions using PCA
+            pca = PCA(n_components=2)
+            vectors_pca = pca.fit_transform(vectors)
+
+            # Display word embedding visualization
+            word_embedding_visualization(vectors_pca, word_list)
+
+display_footer()
---
+++ util.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def word_embedding_visualization(vectors_pca, word_list):
+    fig, axes = plt.subplots(1, 1, figsize=(3, 3))
+
+    # Scatter plot
+    axes.scatter(vectors_pca[:, 0], vectors_pca[:, 1])
+
+    # Annotating each point with its corresponding word
+    for i, word in enumerate(word_list):
+        axes.annotate(word, (vectors_pca[i, 0] + .02, vectors_pca[i, 1] + .02))
+
+    # Set title
+    axes.set_title('Word Embeddings')
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+
+def display_footer():
+    footer = """
+        <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: transparent;
+            text-align: center;
+            color: grey;
+            padding: 10px 0;
+        }
+        </style>
+        <div class="footer">
+            Made with ❤️ by <a href="mailto:zeeshan.altaf@92labs.ai">Zeeshan</a>.
+            Source code <a href='https://github.com/mzeeshanaltaf/streamlit-iss-tracker'>here</a>.</div> 
+        </div>
+    """
+    st.markdown(footer, unsafe_allow_html=True)
